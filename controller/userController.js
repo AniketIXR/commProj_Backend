39e2b0a72402c6a9780348e5e6ff353d9492592b
AniketIXR <aniketroy2003@gmail.com>
2023-05-26 20:44:56 +0530

    some minor bug fix

--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,7 @@ const USER = require('../models/userModel');
 const ApiFeatures = require('../Utils/apiFeatures');
 const catchAsync = require('../Utils/catchAsync');
 
-exports.createUser =catchAsync( async (req,res)=>{
+exports.createUser =catchAsync( async (req,res,next)=>{
      const newUser = await USER.create(req.body);
     
      res.status(200).json({
@@ -16,7 +16,7 @@ exports.createUser =catchAsync( async (req,res)=>{
 });
 
 //We dont need this in user but for reference of ApiFeatures i have addeed this
-exports.getAllData =catchAsync(async(req, res) => {
+exports.getAllData =catchAsync(async(req, res,next) => {
   
         const features = new ApiFeatures(USER.find(),req.query)
         .filter()
@@ -32,7 +32,7 @@ exports.getAllData =catchAsync(async(req, res) => {
    
 });
 
-exports.updateUser = catchAsync(async(req, res) => {
+exports.updateUser = catchAsync(async(req, res,next) => {
     const user = await USER.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators : true,
